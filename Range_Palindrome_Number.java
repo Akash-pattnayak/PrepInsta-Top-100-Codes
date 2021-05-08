569895222b2b134546c8ea4e726a63c4969cdd9e
SoumyaranjanRout7 <routsoumyaranjan2000@gmail.com>
2021-05-08 13:44:29 +0530

    Renaming the file

---
+++ Range_Palindrome_Number.java
@@ -0,0 +1,34 @@
+/**
+ * In this program we try to print the palindrome number within a given range
+ */
+
+import java.io.*;
+
+public class Range_Palindrome_Number {
+    public static void main(String[] args)throws IOException {
+        BufferedReader ob = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the minrange:");
+        int minRange = Integer.parseInt(ob.readLine());
+        System.out.println("Enter the maxrange:");
+        int maxRange = Integer.parseInt(ob.readLine());
+
+        for (int i = minRange;i <= maxRange ; i++) {
+            int revNumber = 0;
+            int temp = i;
+            int n = i;
+            while (n!=0) {
+                int remainder = n % 10;
+                revNumber = revNumber*10+remainder;
+                n = n/10;
+            }
+            if (revNumber == temp) {
+                System.out.println(" "+ temp);
+            }
+        }
+    }
+}
+                // @ if we will use i in while loop then the value of i will change.
+                // @ and i will becom 0 which will create error in for loop,because i should be minRange and after 
+                // @ execution of while loop i would be minRange+1 but due to while loop i becomes 0
+                // @ So to avoid this error we can use another variable to store the value of i which will not impact on value
+                // @ the value of i.
