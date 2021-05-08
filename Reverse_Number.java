6588ac611fcd85fea5e10e5f31ced00e8919e258
SoumyaranjanRout7 <routsoumyaranjan2000@gmail.com>
2021-05-08 12:34:23 +0530

    Adding Comment

--- Reverse_Number.java
+++ Reverse_Number.java
@@ -1,3 +1,16 @@
+/**
+ * #   In this program, we will create a java program to reverse a number. Here we will ask the user to 
+ * # enter a number and then by the help of the java code we will reverse the number and display it on 
+ * # the screen.
+ 
+ * @ Reversing of a number generally means printing the number in reverse order. 
+
+ * $ On most of the sites, the reverse of a number is done in wrong way because if you put 90, 100, 1300 and many 
+ * $ other inputs which have ‘ 0 ’ in last digits of the number then it will neglect 0 in their output. So, below 
+ * $ mentioned code is made after considering all the cases.
+ */
+
+
 import java.io.*;
 
 public class Reverse_Number {
@@ -12,7 +25,6 @@ public class Reverse_Number {
             // revNumber = revNumber * 1o +rem;
             System.out.print(" " +rem);
             Number = Number/10;
-
         }
         System.out.println(" ");
     }
