bedad69c659ae5f1e9ad20f0f63983b933e17556
Ambitious Programmer <66109139+AmbitiousProgrammer7@users.noreply.github.com>
2021-05-09 13:02:45 +0530

    This fixes #9
    I have just formatted my code and also added if else statement according to your recommendation.

--- FibonacciSeries_nth_Term.java
+++ FibonacciSeries_nth_Term.java
@@ -8,26 +8,27 @@ public class FibonacciSeries_nth_Term {
         int prev = 0;
         int current = 1;
         if (term > 1) {
-           if (term == 2) {
-              System.out.print("Fibonacci series upto "+ term +"nd terms  are:");
-            
-        } else if (term == 3) {
-            
-            System.out.print("Fibonacci series upto "+ term +"rd terms  are:");
-        } else{
-            
-            System.out.print("Fibonacci series upto "+ term +"th terms  are:");
-        }
-        for (int i = 1; i <= term ; i++) {
-           System.out.print(" " +prev);
-           int next = current + prev;
-           prev = current;
-           current =  next;
-        }
-           } else {
+            if (term == 2) {
+                System.out.print("Fibonacci series upto " + term + "nd terms  are:");
+
+            } else if (term == 3) {
+
+                System.out.print("Fibonacci series upto " + term + "rd terms  are:");
+            } else {
+
+                System.out.print("Fibonacci series upto " + term + "th terms  are:");
+            }
+            for (int i = 1; i <= term; i++) {
+                System.out.print(" " + prev);
+                int next = current + prev;
+                prev = current;
+                current = next;
+            }
+        } else {
             System.out.println("Wrong Input !! Try again");
             System.out.println("Term value can not be less than 2");
         }
+
         System.out.println(" ");
         sc.close();
     }
