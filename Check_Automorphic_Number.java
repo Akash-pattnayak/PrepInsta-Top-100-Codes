65ba32db274d41d0a2dffb1330e44a86db49a706
SoumyaranjanRout7 <routsoumyaranjan2000@gmail.com>
2021-05-12 13:04:55 +0530

    Adding Comment

--- Check_Automorphic_Number.java
+++ Check_Automorphic_Number.java
@@ -1,7 +1,17 @@
 import java.io.*;
-
 /**
- * Check_Automorphic_Number
+ * ! Automorphic number is a number whose square ends in the same digits as the number itself. 
+
+ * ! For better understanding lets consider an example :
+ * $     Suppose a number 5.
+ * $     Square of 5 is 25.
+ * $    and we see that 25 ends in the same digit as the number itself, means 5.
+
+ * % Hence we can conclude that 5 is an Automorphic Number.
+
+ * % Some more Automorphic Numbers are : 5 , 6 , 76 , 376 
+ * % and 890625.
+
  */
 public class Check_Automorphic_Number {
 
