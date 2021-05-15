beb8953a04c546c75530bdd75793ab0327a1b2ef
SoumyaranjanRout7 <routsoumyaranjan2000@gmail.com>
2021-05-15 07:34:38 +0530

    Replacing variable

---
+++ Section_2/Binary_to_Decimal.java
@@ -0,0 +1,18 @@
+// import java.util.*;
+// public class Binary_to_Decimal {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a binary number:");
+//         int binaryNumber = sc.nextInt();
+//         binaryToDecimal(binaryNumber);
+
+//     }
+//     public static void binaryToDecimal(int Number)
+//     {
+//         int power = 0;
+//         while (Number !=0) {
+//             int rem = Number %10;
+//             int DecimalNumber= rem * 9;
+//         }
+//     }
+// }
